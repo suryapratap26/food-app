54a61500debe73ffc17de075259a739eebf82195
suryapratap26 <suryapratapsinghchundawat66@gmail.com>
2025-10-18 18:57:16 +0530

    commit

--- foodapp/src/main/java/com/kspk/config/SecurityConfig.java
+++ foodapp/src/main/java/com/kspk/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config=new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", frontendUrl));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://kspkfoods.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("Authorization","Content-Type"));
         config.setAllowCredentials(true);
--- foodies/src/service/apiClient.js
+++ foodies/src/service/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const apiClient = axios.create({
     baseURL: API_URL,
