c3506bbb59d68c60e6841f6f06041b7da6e25884
suryapratap26 <suryapratapsinghchundawat66@gmail.com>
2025-10-26 17:02:41 +0530

    final commit

--- food-app-backend-js/src/routes/order.routes.js
+++ food-app-backend-js/src/routes/order.routes.js
@@ -4,15 +4,12 @@ import { checkAdminRole } from '../middleware/admin.middleware.js';
 
 const router = express.Router();
 
-// PUBLIC / CLIENT verification
 router.post('/verify', orderController.verifyPayment);
 
-// All subsequent routes are protected globally
 router.post('/create', orderController.createOrderWithPayment);
 router.get('/', orderController.getUserOrder);
 router.delete('/:orderId', orderController.removeOrder);
 
-// ADMIN ENDPOINTS
 router.get('/all', checkAdminRole, orderController.getOrdersOfAllUsers);
 router.put('/:orderId', checkAdminRole, orderController.updateOrder);
 
