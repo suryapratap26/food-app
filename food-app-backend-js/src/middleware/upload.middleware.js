1f1778d1632042e8190af20c694a9890d41fbffb
suryapratap26 <suryapratapsinghchundawat66@gmail.com>
2025-10-26 11:17:06 +0530

    final commit

--- food-app-backend-js/src/middleware/upload.middleware.js
+++ food-app-backend-js/src/middleware/upload.middleware.js
@@ -1,8 +1,8 @@
 import multer from 'multer';
 
 const upload = multer({
-  dest: 'uploads/',
-  limits: { fileSize: 10 * 1024 * 1024 },
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
 });
 
 export default upload.single('file');
